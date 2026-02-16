b5ffb7b220ec17609c7bad5f061908831f6538f2
jagadish.pokharel <jagupok@gmail.com>
2026-02-16 13:02:49 +0545

    fixed

--- src/Register.tsx
+++ src/Register.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createUserWithEmailAndPassword, sendEmailVerification, signOut } from "firebase/auth";
 import { auth } from './firebase';

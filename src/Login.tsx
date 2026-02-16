b7eef0b463141137aa5ef084033a739d67cd26c0
jagadish.pokharel <jagupok@gmail.com>
2026-02-16 13:02:17 +0545

    fixed

--- src/Login.tsx
+++ src/Login.tsx
@@ -1,4 +1,4 @@
-import React, { useState, ChangeEvent, useEffect } from 'react';
+import React, { useState, useEffect } from 'react';
 import { auth } from './firebase';
 import { signInWithEmailAndPassword } from "firebase/auth";
 import 'bootstrap/dist/css/bootstrap.min.css';
